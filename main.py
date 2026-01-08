619bb22c3a09e796f5617f889e2c05fd0eb254fe
veerabhadragalate01-max <veerabhadragalate01@gmail.com>
2026-01-08 12:21:22 +0530

    supabase

--- main.py
+++ main.py
@@ -23,26 +23,62 @@
 
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse,JSONResponse,RedirectResponse
+from supabase import create_client
 
+db_url="https://tafdsjfjdppwfffkbtqy.supabase.co"
+db_api="sb_publishable_sre0-TYPssYKIQeuJxjIWw_B6UE8b9_"
+db=create_client(db_url,db_api)
 app=FastAPI()
+# contacts=[
+#     {
+#     "cont-id":1,
+#     "name":"veera",
+#     "phno":6281748974
+#     },
+#     {
+#      "cont-id":2,
+#      "name":"amar",
+#      "phno":848977448
+#     }   
+#     ]
 
-contacts=[
-    {
-    "name":"veera",
-    "phno":6281748974
-    },
-    {
-     "name":"amar",
-     "phno":848977448
-    }   
-    ]
+@app.post('/add/contact')
+async def add_contacts(request :Request):
+    data =  await request.json()
+    result=  db.table('contact_table').insert(data).execute()
+    return "sucess"
 
-@app.post('/add/contacts')
-async def add_tasks(request :Request):
-    data = request.json
-    contacts.append(data)
-    return contacts
+@app.get('/contact')
+def get_all_contact_by_id(contact_id):
+    result=  db.table('contact_table').select('*').eq('id',contact_id).execute()
+    data = result.data
+    return data
+    
+        
+#  @app.get("/contact/{cont_id}")
+# def get_contact_by_id(cont_id: int):
+#     for c in contacts:
+#         if c["cont"] == cont_id:
+#             return c   
 
 @app.get('/contacts')
-def get_all_tasks():
+def get_all_contacts():
+    result=db.table('contact_table').select('*').execute()
+    contacts=result.data
     return contacts
+    
+
+@app.put('/contact/{contact_id}')
+async def update_contacts(request :Request,contact_id):
+    data =  await request.json()
+    result=  db.table('contact_table').update(data).eq('id',contact_id).execute()
+    return "upadated sucessfully"
+    
+            
+        
+    
+@app.delete('/contacts/{contacts_id}')
+def delete_contacts(contacts_id):
+    result=  db.table('contact_table').delete().eq('id',contacts_id).execute()
+    return "deleted succesfully"
+    
